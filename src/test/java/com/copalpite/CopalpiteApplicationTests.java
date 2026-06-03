43f72a43a49dbf1d2ff272a00b74fe9e1179a077
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-06-02 23:52:12 -0300

    commit inicial apenas a parte feita pelo spring initializr

---
+++ src/test/java/com/copalpite/CopalpiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copalpite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopalpiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
